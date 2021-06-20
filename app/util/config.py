d7f0a6eaa5922428d85edf11a66c1acd61235824
vprak <vprak@sertiscorp.com>
2021-06-20 15:43:22 +0700

    Stop API when TOTAL_CARD is not even number

--- app/util/config.py
+++ app/util/config.py
@@ -1,9 +1,14 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 
 # dotenv doesn't override existing environment from docker-compose.yml
 load_dotenv(".env")
+
+if int(os.environ.get('TOTAL_CARD')) % 2 != 0:
+  print("Process exit: TOTAL_CARD value must be even number.")
+  sys.exit(1)
 
 MONGODB_URL = os.environ.get('MONGODB_URL')
 TOTAL_CARD = int(os.environ.get('TOTAL_CARD'))
